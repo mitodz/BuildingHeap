4e0e59e8cd6bc90ace0876c97ce028264c6843d6
mitodz <mitodzzz@gmail.com>
2019-04-17 21:02:24 +0300

    Начало

--- src/Heap.java
+++ src/Heap.java
@@ -1,7 +1,17 @@
 public class Heap {
 
-    public long parent (long i) {
-        return (long)Math.floor((i - 1)/2);
+
+
+    public long parent(long i) {
+        return (long) Math.floor((i - 1) / 2);
+    }
+
+    public long left(long i) {
+        return 2 * i + 1;
+    }
+
+    public long right(long i) {
+        return 2 * i + 2;
     }
 
 }

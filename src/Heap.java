7ca7d677e0ce6a6ff12cd53ca8468652908d4d6e
mitodz <mitodzzz@gmail.com>
2019-04-17 21:33:14 +0300

    Начало

--- src/Heap.java
+++ src/Heap.java
@@ -1,16 +1,32 @@
 public class Heap {
+    private int[] H;
 
-
-
-    public long parent(long i) {
-        return (long) Math.floor((i - 1) / 2);
+    public Heap(int[] H) {
+        this.H = H;
     }
 
-    public long left(long i) {
+    public void siftUp (int i) {
+        while (i > 1 && H[parent(i)]<H[i]) {
+            swap(H, i);
+            i = parent(i);
+        }
+    }
+
+    public void swap (int[] H, int i) {
+        int t = H[i];
+        H[i] = H[parent(i)];
+        H[parent(i)] = t;
+    }
+
+    public int parent(int i) {
+        return (int) Math.floor((i - 1) / 2);
+    }
+
+    public int left(int i) {
         return 2 * i + 1;
     }
 
-    public long right(long i) {
+    public int right(int i) {
         return 2 * i + 2;
     }
 

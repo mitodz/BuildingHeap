7b5ee0d4000c74d2874868784ba5ed4bee1c57e5
mitodz <mitodzzz@gmail.com>
2019-04-18 21:48:36 +0300

    Начало

--- src/Heap.java
+++ src/Heap.java
@@ -1,8 +1,13 @@
 public class Heap {
-    private int[] H;
+    private int maxSize = Integer.MAX_VALUE;
+    private int[] H = new int[maxSize];
+    private int size;
 
     public Heap(int[] H) {
-        this.H = H;
+        for (int i = 0; i < H.length; i++) {
+            this.H[i] = H[i];
+        }
+        size = H.length;
     }
 
     public void siftUp (int i) {
@@ -12,10 +17,65 @@ public class Heap {
         }
     }
 
+    public void siftDown (int i) {
+        int maxIndex = i;
+        int l = left(i);
+        if (l<=size && H[l] > H[maxIndex]) {
+            maxIndex = l;
+        }
+        int r = right(i);
+        if (r<=size && H[r]>H[maxIndex]) {
+            maxIndex = r;
+        }
+        if (i!=maxIndex) {
+            swap(i, maxIndex);
+            siftDown(maxIndex);
+        }
+    }
+
+    public void insert (int p) {
+        if (size==maxSize) {
+            return;
+        }
+        size++;
+        H[size] = p;
+        siftUp(size);
+    }
+
+    public int extractMax(){
+        int result = H[0];
+        H[0] = H[size - 1];
+        size--;
+        siftDown(0);
+        return result;
+    }
+
+    public void remove (int i) {
+        H[i] = Integer.MAX_VALUE;
+        siftUp(i);
+        extractMax();
+    }
+
+    public void changePriority (int i, int p) {
+        int oldP = H[i];
+        H[i] = p;
+        if (p>oldP) {
+            siftUp(i);
+        } else {
+            siftDown(i);
+        }
+    }
+
     public void swap (int i) {
         int t = H[i];
         H[i] = H[parent(i)];
         H[parent(i)] = t;
+    }
+
+    public void swap (int i, int j) {
+        int t = H[i];
+        H[i] = H[j];
+        H[j] = t;
     }
 
     public int parent(int i) {
